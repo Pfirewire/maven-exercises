03c3bf2378aebc2c3d986493531f43634476b91f
Stephen Pflug <stephen@pflug.us>
2022-08-30 15:10:20 -0500

    maven exercises complete

--- src/main/java/com/codeup/StringManipulation.java
+++ src/main/java/com/codeup/StringManipulation.java
@@ -1,7 +1,22 @@
-package src.main.java.com.codeup;
+package com.codeup;
+
+import util.Input;
+
+import java.io.Console;
+import java.util.*;
+import org.apache.commons.lang3.StringUtils;
 
 public class StringManipulation {
     public static void main(String[] arg) {
-        System.out.println("Hello, World!");
+        Input inpt = new Input();
+        String userInput = inpt.getString("Enter a string: ");
+        System.out.println("You entered: " + userInput);
+        if(!StringUtils.isNumeric(userInput)) {
+            System.out.println(userInput + " is not a number.");
+            System.out.println(StringUtils.swapCase(userInput));
+            System.out.println(StringUtils.reverse(userInput));
+        } else {
+            System.out.println(userInput + " is a number!");
+        }
     }
 }
